c34bbb984ef656d856e3aa915e325b6c24f7f739
Sunit <sunit0095@gmail.com>
2024-03-03 21:02:01 -0500

    Queue overhaul

--- queue.c
+++ queue.c
@@ -1,103 +1,118 @@
 #include "queue.h"
 
 
-
 int main(){
 
-    Peek();
-    Enqueue(7);
-    Peek();
+    Queue_Type* q1 = create(q1);
+    Peek(q1);
+    Enqueue(q1, 7);
+    Peek(q1);
 
     int dequeued_val;
-    Dequeue(&dequeued_val);
-    Peek();
+    Dequeue(q1, &dequeued_val);
+    Peek(q1);
 
-    Dequeue(&dequeued_val);
-    Enqueue(2);
-    Enqueue(4);
-    Peek();
+    Dequeue(q1, &dequeued_val);
+    Enqueue(q1, 2);
+    Enqueue(q1, 4);
+    Peek(q1);
+    Dequeue(q1, &dequeued_val);
 
     return 0;
 }
 
-void Enqueue (int val){
+Queue_Type* create(Queue_Type* q){
+
+    q = (Queue_Type*) malloc (sizeof(Queue_Type));
+    
+    if (NULL == q){
+
+        printf("create() unsuccessful due to malloc failure\n");
+        return NULL;
+    }
+    q->first = NULL;
+    q->last = NULL;
+    return q;
+}
+
+void Enqueue (Queue_Type* q, int val){
 
     QUEUEMEMBER* qmember = (QUEUEMEMBER*) malloc (sizeof(QUEUEMEMBER));
 
     if (NULL == qmember){
 
-        printf ("Enqueue unsuccessful due to malloc() failure.\n");
+        printf ("Enqueue() unsuccessful due to malloc failure.\n");
         return;
     }
 
     qmember->data = val;
     qmember->ptr = NULL;
     
-    if (NULL == first){
+    if (NULL == q->first){
         
-        first = qmember;
+        q->first = qmember;
     }
-    if (NULL == last){
+    if (NULL == q->last){
 
-        last = first;
+        q->last = q->first;
     }
     else{
 
-        last->ptr = qmember;
-        last = qmember;
+        q->last->ptr = qmember;
+        q->last = qmember;
     }
 
     printf("Queue after Enqueue():\n");
-    Print(first);
+    Print(q);
     return;
 }
 
-void Dequeue (int* dqd_val){
+void Dequeue (Queue_Type* q, int* dqd_val){
 
-    if (NULL == first){
+    if (NULL == q->first){
 
         printf ("Empty Queue. Nothing to Dequeue\n");
         return;
     }
 
-    QUEUEMEMBER* temp = first;
+    QUEUEMEMBER* temp = q->first;
 
-    if (first == last){             // when there's only one item in Queue
+    if (q->first == q->last){             // when there's only one item in Queue
 
-        last = NULL;
+        q->last = NULL;
     }
     
-    first = first->ptr;
+    q->first = q->first->ptr;
 
     *dqd_val = temp->data;
 
     free(temp);
 
     printf("Queue after Dequeue():\n");
-    Print(first);
+    Print(q);
     return;
 }
 
-void Peek(){
+void Peek(Queue_Type* q){
 
-    if (NULL == first){
+    if (NULL == q->first){
         printf("Empty Queue. Nothing to peek\n");
         return;
     }
 
-    printf("First item in Queue = %d\nLast item in Queue = %d\n", first->data, last->data);
+    printf("First item in Queue = %d\nLast item in Queue = %d\n", q->first->data, q->last->data);
 
     return;
 }
 
-void Print(QUEUEMEMBER* beg){
+void Print(Queue_Type* q){
 
-    if (NULL == beg){
+    if (NULL == q->first){
         printf("Empty Queue. Nothing to print\n");
         return;
     }
 
-    QUEUEMEMBER* iter = beg;
+    QUEUEMEMBER* iter = q->first;
 
     while(iter->ptr != NULL){
 
--- queue.h
+++ queue.h
@@ -1,17 +1,21 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
-#include "LL.h"
+#include "LL_shared.h"
 #include <stdio.h>
 
-typedef NODE QUEUEMEMBER;       // alias for NODE
+typedef NODE QUEUEMEMBER;                   // alias for NODE
 
-static QUEUEMEMBER* first = NULL;       // first is the LL head node pointer
-static QUEUEMEMBER* last  = NULL;       // last is the LL tail node pointer
+typedef struct Queue{
+    QUEUEMEMBER* first;
+    QUEUEMEMBER* last;
+} Queue_Type;
 
-void Enqueue (int);
-void Dequeue (int*);
-void Peek (void);
-void Print(QUEUEMEMBER*);                   // for debug purposes   // expects first pointer
+
+Queue_Type* create (Queue_Type*);
+void Enqueue (Queue_Type*, int);
+void Dequeue (Queue_Type*, int*);
+void Peek (Queue_Type*);
+void Print(Queue_Type*);                   // for debug purposes   
 
 #endif              // QUEUE_H
